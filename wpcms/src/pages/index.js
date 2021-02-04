78a2a99be53eac7592d41a98b7e90b50934a3b31
Alestry Pérez <alestry.perez@hyperisland.se>
2021-02-04 23:42:49 +0100

    commit

---
+++ wpcms/src/pages/index.js
@@ -0,0 +1,6 @@
+// If you don't want to use TypeScript you can delete this file!
+import React, { Component } from "react"
+import { PageProps, Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Image from "../components/image"
